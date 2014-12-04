1165cd2b12ae30f36e066c1474c3f1493943825b
Modulus <thejohnskauge@gmail.com>
2014-12-04 20:41:31 +0100

    Jadda

--- src/main/java/com/eguaks/demo/Main.java
+++ src/main/java/com/eguaks/demo/Main.java
@@ -1,12 +1,19 @@
 package com.eguaks.demo;
 
-import static spark.Spark.get;
+import static spark.Spark.*;
 
 /**
  * Created by Modulus on 04.12.2014.
  */
 public class Main {
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
+        get("/", (req, res) -> "Hello World");
+
+        post("/", (req, res) -> "Post called!");
+
+
+        put("/", (req, res) -> "Post called!");
+
+        delete("/", (req, res) -> "Delete called!");
     }
 }
